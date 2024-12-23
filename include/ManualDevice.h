bacc6590cf693bdcb450dbc8b86f6135252204d0
RenatoZanolla <rena.zanolla@gmail.com>
2024-12-23 12:34:22 +0100

    Update ManualDevice.h

---
+++ include/ManualDevice.h
@@ -0,0 +1,26 @@
+#ifndef MANUALDEVICE_H_
+#define MANUALDEVICE_H_
+
+#include "Device.h"
+
+using namespace std;
+
+class ManualDevice:public Device{
+private:
+    string name;    //nome del dispositivo.
+    int id; //identificativo unico.
+    float power;    //consumo (o produzione) energetico.
+    bool isOn;  //stato del dispositivo.
+    optional<pair<int, int>> timer;   //orario di accensione/spegnimento.
+    float energyConsumed;   //consumo energetico totale
+
+public:
+    virtual void turnOn();  //accende il dispositivo.
+    virtual void turnOff(); //spegne il dispositivo.
+    virtual void update(int currentTime);   //aggiorna lo stato del dispositivo.
+    void updateEnergy(int minutesElapsed);   //aggiorna il consumo energetico accumulato da un dispositivo
+    void setTimer(optional<pair<int, int>>);
+};
+
+#endif
+
