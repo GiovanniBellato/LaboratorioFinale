e4b983a4810ab27811048b0f0c62ce5b9dcb1374
GiovanniBellato <96789638+GiovanniBellato@users.noreply.github.com>
2024-12-23 10:44:31 +0100

    cmake

---
+++ build/CMakeFiles/ProgettoFinale.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/giovanni/coding/universita_programmazione/LaboratorioFinale/src/CycleDevice.cpp" "CMakeFiles/ProgettoFinale.dir/src/CycleDevice.cpp.o" "gcc" "CMakeFiles/ProgettoFinale.dir/src/CycleDevice.cpp.o.d"
+  "/Users/giovanni/coding/universita_programmazione/LaboratorioFinale/src/Device.cpp" "CMakeFiles/ProgettoFinale.dir/src/Device.cpp.o" "gcc" "CMakeFiles/ProgettoFinale.dir/src/Device.cpp.o.d"
+  "/Users/giovanni/coding/universita_programmazione/LaboratorioFinale/src/DomoticSystem.cpp" "CMakeFiles/ProgettoFinale.dir/src/DomoticSystem.cpp.o" "gcc" "CMakeFiles/ProgettoFinale.dir/src/DomoticSystem.cpp.o.d"
+  "/Users/giovanni/coding/universita_programmazione/LaboratorioFinale/src/ManualDevice.cpp" "CMakeFiles/ProgettoFinale.dir/src/ManualDevice.cpp.o" "gcc" "CMakeFiles/ProgettoFinale.dir/src/ManualDevice.cpp.o.d"
+  "/Users/giovanni/coding/universita_programmazione/LaboratorioFinale/src/main.cpp" "CMakeFiles/ProgettoFinale.dir/src/main.cpp.o" "gcc" "CMakeFiles/ProgettoFinale.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
