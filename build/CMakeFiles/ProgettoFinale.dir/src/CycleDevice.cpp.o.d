118e8b25857dc41bc357f7016e878cb8c004fd16
GiovanniBellato <96789638+GiovanniBellato@users.noreply.github.com>
2024-12-23 16:11:29 +0100

    Delete build directory

--- build/CMakeFiles/ProgettoFinale.dir/src/CycleDevice.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ProgettoFinale.dir/src/CycleDevice.cpp.o: \
-  /Users/giovanni/coding/universita_programmazione/LaboratorioFinale/src/CycleDevice.cpp
--- build/CMakeFiles/ProgettoFinale.dir/src/ManualDevice.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/ProgettoFinale.dir/src/ManualDevice.cpp.o: \
-  /Users/giovanni/coding/universita_programmazione/LaboratorioFinale/src/ManualDevice.cpp
