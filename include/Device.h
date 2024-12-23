89ed713d036b4e7f8bafdce0dac538a8edbf027b
RenatoZanolla <rena.zanolla@gmail.com>
2024-12-23 17:34:45 +0100

    Update Device.h

--- include/Device.h
+++ include/Device.h
@@ -12,15 +12,16 @@ private:
     string name;    //nome del dispositivo.
     int id; //identificativo unico.
     float power;    //consumo (o produzione) energetico.
+    float total_power; //consumo totale energetico
     bool isOn;  //stato del dispositivo.
     Time timer_on;   //orario di accensione.
+    bool timer = false;
     float energyConsumed;   //consumo energetico totale
 
 protected:
-    virtual void turnOn();  //accende il dispositivo.
-    virtual void turnOff(); //spegne il dispositivo.
-    virtual void update(Time currentTime);   //aggiorna lo stato del dispositivo.
-    void updateEnergy(Time currentTime);   //aggiorna il consumo energetico accumulato da un dispositivo
+    virtual void turnOn() = 0;  //accende il dispositivo.
+    virtual void turnOff() = 0; //spegne il dispositivo.
+    virtual void update(Time currentTime) = 0;   //aggiorna lo stato del dispositivo.
 };
 
 #endif
