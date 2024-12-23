0a4027681bd2e544fc1aeb9f7089d2806708cb85
RenatoZanolla <rena.zanolla@gmail.com>
2024-12-23 15:00:21 +0100

    Update Device.cpp

---
+++ src/Device.cpp
@@ -0,0 +1,28 @@
+#ifndef DEVICE_H_
+#define DEVICE_H_
+
+#include "Time.h"
+
+#include <string>
+
+using namespace std;
+
+class Device{
+private:
+    string name;    //nome del dispositivo.
+    int id; //identificativo unico.
+    float power;    //consumo (o produzione) energetico.
+    bool isOn;  //stato del dispositivo.
+    Time timer_on;   //orario di accensione.
+    float energyConsumed;   //consumo energetico totale
+
+protected:
+    virtual void turnOn(){
+      
+    }
+    virtual void turnOff(); //spegne il dispositivo.
+    virtual void update(int currentTime);   //aggiorna lo stato del dispositivo.
+    void updateEnergy(int minutesElapsed);   //aggiorna il consumo energetico accumulato da un dispositivo
+};
+
+#endif
