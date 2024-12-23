578e34ebcea9ed0494ca9faf45c17ae6af8f22b7
RenatoZanolla <rena.zanolla@gmail.com>
2024-12-23 15:43:04 +0100

    Update Time.cpp

--- src/Time.cpp
+++ src/Time.cpp
@@ -59,3 +59,31 @@ bool Time::operator <= (Time* time) {
         return true;
     return false;
 }
+
+Time Time::operator + (Time* time){
+    Time new_time;
+    new_time.hour = hour + time->hour;
+    if(minute + time->minute > 60){
+        new_time.hour ++;
+        new_time.minute = (minute + time->minute) - 60;
+    }
+    else{
+        new_time.minute = minute + time->minute;
+    }
+    if(new_time.hour > 23)
+        throw std::invalid_argument("Time is out of range");
+    return new_time;
+}
+
+Time Time::operator - (Time* time){
+    Time new_time;
+    new_time.hour = hour - time->hour;
+    new_time.minute = minute - time->minute;
+    if(new_time.minute < 0){
+        new_time.hour --;
+        new_time.minute += 60;
+    }
+    if(time.hour < 0)
+        new_time.hour = hour;
+    return new_time;
+}
