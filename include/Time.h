f4dfb06479a9378e97374358516a3992c9fd8ffc
Marco Della Putta <39233605+Marco-Della-Putta@users.noreply.github.com>
2025-01-11 16:06:29 +0100

    Update Time.h

--- include/Time.h
+++ include/Time.h
@@ -9,14 +9,15 @@ private:
     int hour;
     int minute;
 public:
+
     Time();
     Time(int hour, int minute);
     Time getTime();
     void setTime(int hour, int minute);
-    bool operator > (Time* time);
-    bool operator >= (Time* time);
-    bool operator < (Time* time);
-    bool operator <= (Time* time);
+    bool operator > (Time time);
+    bool operator >= (Time time);
+    bool operator < (Time time);
+    bool operator <= (Time time);
     Time operator+(const Time& time) const;
     Time operator-(const Time& time) const;
     Time& operator = (Time& time);
