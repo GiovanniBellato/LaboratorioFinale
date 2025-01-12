9e000410bdaed0327574437be286457cad1bf07e
Marco Della Putta <39233605+Marco-Della-Putta@users.noreply.github.com>
2025-01-12 18:16:01 +0100

    Update Device.h

--- include/Device.h
+++ include/Device.h
@@ -1,8 +1,3 @@
-#ifndef DEVICE_H_
-#define DEVICE_H_
-
-#include "Time.h"
-
 #include <string>
 
 class Device{
@@ -15,22 +10,21 @@ private:
     Time timer_on;   //orario di accensione.
     bool timer = false;
     float energyConsumed;   //consumo energetico totale
-    
+
 public:
-    
+
     Device() : timer_on(0, 0) {}
     virtual ~Device() = default;
-    
+
     virtual void update(Time currentTime) = 0;   //aggiorna lo stato del dispositivo.
     virtual void turnOn(Time) = 0;  //accende il dispositivo.
     virtual void turnOff(Time) = 0; //spegne il dispositivo.
     virtual std::string getName() const = 0;
     virtual float getPower() = 0;
-    virtual float getConsumption() = 0;
+    virtual float getConsumption(Time) = 0;
     virtual void removeTimer(Time) = 0;
     virtual void setTimerOn(Time, Time) = 0;
     virtual void setTimerOff(Time, Time) = 0;
 };
 
 #endif
-
