e0daccc3b9f71d022814335896bce23c43140480
RenatoZanolla <rena.zanolla@gmail.com>
2025-01-13 18:29:34 +0100

    Update CycleDevice.cpp

--- src/CycleDevice.cpp
+++ src/CycleDevice.cpp
@@ -18,7 +18,7 @@ void CycleDevice::turnOn(Time current_time){  //accende il dispositivo.
     	started = true;
         start = current_time;
         isOn = true;
-        std::cout << "[" << current_time.toString() << "] Il dispositivo <" << name << "> si è acceso.";
+        std::cout << "[" << current_time.toString() << "] Il dispositivo <" << name << "> si è acceso. \n";
     }
 }
 
@@ -29,7 +29,7 @@ void CycleDevice::turnOff(Time current_time){  //spegne il dispositivo.
         total_power += (power * delta.toHour());
         if(timer && current_time >= timer_on)
             timer = false;
-        std::cout << "[" << current_time.toString() << "] Il dispositivo <" << name << "> si è spento.";
+        std::cout << "[" << current_time.toString() << "] Il dispositivo <" << name << "> si è spento. \n";
     }
 }
 
@@ -45,7 +45,7 @@ void CycleDevice::update(Time current_time){  // Aggiorna lo stato del dispositi
 void CycleDevice::setTimerOn(Time current_time, Time time){
     timer_on = time;
     timer = true;
-    std::cout << "[" << current_time.toString() << "] Impostato un timer di accesione nel dispositivo <" << name << ">"<<" alle <" << current_time.toString() << ">";
+    std::cout << "[" << current_time.toString() << "] Impostato un timer di accesione nel dispositivo <" << name << ">"<<" alle <" << current_time.toString() << "> \n";
 }
 
 void CycleDevice::reset(){
@@ -56,7 +56,7 @@ void CycleDevice::reset(){
 
 void CycleDevice::removeTimer(Time current_time){
     timer = false;
-    std::cout << "[" << current_time.toString() << "] Rimosso il timer dal dispositivo <" << name << ">" ;
+    std::cout << "[" << current_time.toString() << "] Rimosso il timer dal dispositivo <" << name << "> \n" ;
 
 }
 
