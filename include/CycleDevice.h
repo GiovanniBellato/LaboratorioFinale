6eae10b7dd0fc722c2762c76461bb950cb710391
Marco Della Putta <39233605+Marco-Della-Putta@users.noreply.github.com>
2025-01-14 21:10:37 +0100

    Update CycleDevice.h

--- include/CycleDevice.h
+++ include/CycleDevice.h
@@ -5,13 +5,15 @@
 
 class CycleDevice : public Device {
 private:
-    std::string name;    //nome del dispositivo.
-    int id; //identificativo unico.
-    float power;    //consumo (o produzione) energetico.
-    bool isOn = false;  //stato del dispositivo.
-    Time start;         // Orario di accensione
-    Time duration; // Durata prefissata del ciclo
-    float total_power = 0;   // Consumo totale energetico
+    std::string name;    
+    int id;   //IDENTIFICATIVO UNICO
+    float power;    //CONSUMO (O PRODUZIONE) ENERGETICO
+    bool isOn = false;  //STATO DEL DISPOSITIVO
+    float total_power = 0;  //CONSUMO TOTALE EFFETTUATO
+
+    //VARIABILI PER LA GESTIONE DEI TIMER & DURATA CICLO
+    Time start;         
+    Time duration;   
     Time timer_on;
     bool timer = false;
     bool started = false;
@@ -22,17 +24,20 @@ public:
 
     virtual ~CycleDevice() = default;
 
-    void turnOn(Time current_time) override;  //accende il dispositivo.
-    void turnOff(Time current_time) override;  //spegne il dispositivo.
-    void update(Time current_time) override;  // Aggiorna lo stato del dispositivo
+    void turnOn(Time current_time) override;  //ACCENDE
+    void turnOff(Time current_time) override;  //SPEGNE
+    void update(Time current_time) override;  // AGGIORNA IL DISPOSITIVO AL NUOVO 'TEMPO' DATO
+
+    //FUNZIONI PER GESTIONE DEI TIMER
     void setTimerOn(Time current_time, Time time) override;
+    void setTimerOff(Time start, Time end) override;
     void removeTimer(Time) override;
     void reset();
+
+    //FUNZIONI GET PER LA RACCOLTA DI INFORMAZIONI SUL DEVICE
     std::string getName() const override {return name;}
     float getPower() override {return power;}
     float getConsumption(Time) override;
-
-    void setTimerOff(Time start, Time end) override;
 };
 
 #endif
