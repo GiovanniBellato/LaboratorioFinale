3212896d43e05bb29c79b75b106c1656ae72367f
Marco Della Putta <39233605+Marco-Della-Putta@users.noreply.github.com>
2025-01-14 21:30:12 +0100

    Update Interface.h

--- include/Interface.h
+++ include/Interface.h
@@ -7,8 +7,9 @@ class Interface
 {
     public:
         Interface();
-        void commandReader(const std::string& command);
-
+        void commandReader(const std::string& command); 
+        /* IL COMMAND-READER E' LA FUNZIONE PER L'INTERPRETAZIONE DEI COMANDI IN INPUT,
+           E PER IL COLLEGAMENTO DI ESSI ALLE FUNZIONI DI DOMOTICSYSTEM.    */
 };
 
 #endif 
