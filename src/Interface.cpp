8b31e9197e20d215fcdf1f864626ca8e13221f65
GiovanniBellato <96789638+GiovanniBellato@users.noreply.github.com>
2025-01-14 22:25:09 +0100

    commenti interface

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -10,10 +10,15 @@ Interface::Interface(){ /*NULLA*/ }
 
 void Interface::commandReader(const std::string& command)
 {
+<<<<<<< Updated upstream
     //CREO LISTA DEI DISPOSITIVI
     std::vector<std::string> deviceList = sistemaDomotico.getDevices();
 
     //JULIO E' UN OGGETTO CHE ESTRAE I COMANDI DI INPUT UNO A UNO
+=======
+ 
+    //julio e' un oggetto di istringstream che estrae i dati uno a uno da command
+>>>>>>> Stashed changes
     std::istringstream julio(command);
 
     std::vector<std::string> commandVector;
@@ -25,16 +30,24 @@ void Interface::commandReader(const std::string& command)
         commandVector.push_back(word);
     }
 
+<<<<<<< Updated upstream
     //DICHIARAZIONE VARIABILI
     std::string cmd, devicename, action, startTime, stopTime, time, word2, word3;
+=======
+    //creo delle variabili per contenere i dati del comando
+    std::string cmd, devicename, action, startTime, stopTime, time, word2, word3;
+
+    //inizializzo le variabili a zero
+>>>>>>> Stashed changes
     word2 = "0";
     word3 = "0";
 
     try{
 
+    //controllo il primo elemento del vettore per capire quale comando è stato inserito
     if(commandVector[0] == "set")
     {
-
+        //controllo il secondo elemento del vettore per capire quale comando è stato inserito
         if(commandVector[1] == "time")
         {
         	try{
@@ -49,6 +62,7 @@ void Interface::commandReader(const std::string& command)
                 }
                 Time targetTime = toTime(time);
                 
+                //imposto l'orario del sistema
                 sistemaDomotico.setTime(targetTime);
 
         	}catch(...){
@@ -58,6 +72,7 @@ void Interface::commandReader(const std::string& command)
         }
         else
         {
+            //controllo se il comando è stato inserito correttamente
             try{
                 if(commandVector[1] == "Impianto" || commandVector[1] =="impianto")
                 {
@@ -168,7 +183,7 @@ void Interface::commandReader(const std::string& command)
                 std::cerr<<"Errore, nel sistema non esiste un dispositivo con questo nome."<<std::endl;
             }
             
-
+            //controllo che la parola seguente sia "on" o "off" o un orario
             if(word2 == "on" || word2 == "off")
             {
                 action = word2;
@@ -189,6 +204,14 @@ void Interface::commandReader(const std::string& command)
                 stopTime = word3;
 
                 try{
+
+                //controllo se ci sono delle lettere inserite per sbaglio nell'orario
+                if(std::any_of(startTime.begin(), startTime.end(), ::isalpha))
+                {
+                    throw std::string("L'orario inserito non è valido.\n");
+                }
+                
+
                 Time start_t = toTime(startTime);
 
                 if(stopTime != "0")
@@ -196,19 +219,28 @@ void Interface::commandReader(const std::string& command)
                     if(sistemaDomotico.checkDevice(devicename) != 1) {
                         throw std::string("Non è possibile impostare un timer di spegnimento su un CycleDevice.\n");
                     }else{
+
+                    //controllo se ci sono delle lettere inserite per sbaglio nell'orario
+                    if(std::any_of(stopTime.begin(), stopTime.end(), ::isalpha))
+                    {
+                        throw std::string("L'orario inserito non è valido.\n");
+                    }
+
+
                 	Time stop_t = toTime(stopTime);
                 	sistemaDomotico.setTimer(devicename, start_t, stop_t);
                     }
                 }
                 else sistemaDomotico.setTimer(devicename, start_t);
                 }catch(...){
-                	throw std::string("Orario inserito non valido\n");
+                	std::cerr<<"Orario inserito non valido"<<std::endl;
                 }
             }
         }
     }
     else if (commandVector[0] == "rm")
     {
+        //controllo che esista il dispositivo e che lo spelling sia giusto
         try{
                 if(commandVector[1] == "Impianto" || commandVector[1] =="impianto")
                 {
@@ -269,7 +301,7 @@ void Interface::commandReader(const std::string& command)
                 std::cerr<<"Errore, nel sistema non esiste un dispositivo con questo nome."<<std::endl;
             }
 
-
+        //rimuovo il timer del dispositivo
         sistemaDomotico.removeTimer(devicename);
 
     }
@@ -339,11 +371,12 @@ void Interface::commandReader(const std::string& command)
                 std::cerr<<"Errore, nel sistema non esiste un dispositivo con questo nome."<<std::endl;
             }
 
-
+            //mostro lo stato del dispositivo
         	sistemaDomotico.showDeviceStatus(devicename);
         }
         else
         {
+            //mostro lo stato di tutti i dispositivi
             sistemaDomotico.showStatus();
 
         }
@@ -353,19 +386,19 @@ void Interface::commandReader(const std::string& command)
 
         if(commandVector[1] == "time")
         {
+            //resetto l'orario del sistema
             sistemaDomotico.resetTime();
-
         }
         else if (commandVector[1] == "timers")
         {
+            //rimuovo tutti i timer
             for(std::string nome : sistemaDomotico.getDevices())
             	sistemaDomotico.removeTimer(nome);
-
         }
         else if (commandVector[1] == "all")
         {
+            //resetto il sistema
             sistemaDomotico.resetSystem();
-
         }
         else
         {
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main()
 {
 	std::cout<<"**** WARNING : Nell'input degli orari è importante utilizzare il formato hh:mm **** \n"<<std::endl;
 	Interface commandInterface;
+<<<<<<< Updated upstream
     	std::cout<<"Il programma e' pronto. Procedere inserendo i comandi: \n"<< std::endl;
 try{
     while(true)
@@ -14,13 +15,24 @@ try{
         std::getline(std::cin, inputUtente);
 
         if(inputUtente == "exit")
+=======
+    std::cout<<"Il programma e' pronto. Procedere inserendo i comandi: \n"<< std::endl;
+    try{
+        while(true)
+>>>>>>> Stashed changes
         {
-            std::cout<< "Chiusura del programma. Arrivederci!\n" << std::endl;
-            break;
+	        std::cout<<">>";    
+            std::string inputUtente;
+            std::getline(std::cin, inputUtente);
+
+            if(inputUtente == "exit")
+            {
+                std::cout<< "Chiusura del programma. Arrivederci!\n" << std::endl;
+                break;
+            }
+            commandInterface.commandReader(inputUtente);
         }
-        commandInterface.commandReader(inputUtente);
-    }
-}catch(...){ throw std::string("PROGRAMMA CRASHATO."); }
+    }catch(...){ std::cerr<<"PROGRAMMA CRASHATO."<<std::endl; }
     return 0;
     
 }
