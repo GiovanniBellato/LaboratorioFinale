2b6f1fc4acc6506868d806a348a29f8ded5ce5dd
GiovanniBellato <96789638+GiovanniBellato@users.noreply.github.com>
2025-01-14 22:28:14 +0100

    commenti interface

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,27 @@ int main()
 {
 	std::cout<<"**** WARNING : Nell'input degli orari è importante utilizzare il formato hh:mm **** \n"<<std::endl;
 	Interface commandInterface;
-<<<<<<< Updated upstream
-    	std::cout<<"Il programma e' pronto. Procedere inserendo i comandi: \n"<< std::endl;
-try{
-    while(true)
-    {
-	std::cout<<">";    
-        std::string inputUtente;
-        std::getline(std::cin, inputUtente);
-
-        if(inputUtente == "exit")
-=======
     std::cout<<"Il programma e' pronto. Procedere inserendo i comandi: \n"<< std::endl;
     try{
         while(true)
->>>>>>> Stashed changes
         {
-	        std::cout<<">>";    
+	        std::cout<<">";    
             std::string inputUtente;
             std::getline(std::cin, inputUtente);
 
             if(inputUtente == "exit")
             {
-                std::cout<< "Chiusura del programma. Arrivederci!\n" << std::endl;
-                break;
-            }
-            commandInterface.commandReader(inputUtente);
+	            std::cout<<">>";    
+                std::string inputUtente;
+                std::getline(std::cin, inputUtente);
+
+                if(inputUtente == "exit")
+                {
+                    std::cout<< "Chiusura del programma. Arrivederci!\n" << std::endl;
+                    break;
+                }
+                commandInterface.commandReader(inputUtente);
+           }   
         }
     }catch(...){ std::cerr<<"PROGRAMMA CRASHATO."<<std::endl; }
     return 0;
